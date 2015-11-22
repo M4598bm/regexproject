04400bfb89e813b44632e082a7004ab8b1a408c5
Shi Shu <shishu_1998@yahoo.com>
2015-11-22 16:12:13 -0500

    home

--- utils.py
+++ utils.py
@@ -8,10 +8,49 @@ def getResult(query):
         plist.append(r)
 
     url = urllib2.urlopen(plist[0])
-    page = url.read().decode('ascii')
+    page = url.read()
     soup = bs4.BeautifulSoup(page)
     raw = soup.get_text(page)
-    text = re.sub("[\t\n ]+",' ',raw)
-    print text
+    if("who" in query):
+        """
+        finds all text with capitals for 2 consecutive words
+        """
+        text = re.findall("[A-Z][a-z] + [A-Z][a-z]+",raw)
+    elif("where" in query):
+        """
+        finds all text with one capital
+        """
+        text = re.findall("[A-Z][a-z]+",raw)
+    else:
+        """
+        not sure how to do when yet
+        """
+        text = re.findall(" ");
+    dictionary = {}
+    for result in text:
+        """
+        if the result has been found before, add 1 to number of occurence
+        """
+        if name in dictionary.keys():
+            dictionary[result] += 1
+        """
+        if the result hasn't been found before, set the occurence equal to one
+        """
+        else:
+            dictionary[result] = 1
+    """
+    testing purposes
+    """
+    for key in dictionary.keys():
+        print "%s:%d" % (key,dictionary[key])
+    return findResult(dictionary)
+        
+def findResult(dictionary):
+    result = dictionary.keys()[0]
+    for key in dictionary.keys():
+        if(key.get() > result.get()):
+            result = key
+    return result
 
-getResult("penguins")
+#getResult("Penguin")
+findResult({"a":1,"b":2,"c":3})
