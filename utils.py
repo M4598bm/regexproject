d02c893da5a6ea4dbd29bc43307bd5ae43c538b9
shishu1998 <shishu_1998@yahoo.com>
2015-11-21 21:49:09 -0500

    testing

--- utils.py
+++ utils.py
@@ -1,15 +1,17 @@
-import google,urllib2,bs4,re
+import google, urllib2, bs4, re
 
 def getResult(query):
-    pages = google.search(query,num = 10,start = 0, stop = 10)
+    pages = google.search(query,num=10,start=0,stop=10)
+
     plist = []
     for r in pages:
         plist.append(r)
-    url = urllib2.urlopen(plist[2])
-    page = url.read().decode()
-    soup = bs4.BeautifulSoup(page,'html.parser')
+
+    url = urllib2.urlopen(plist[0])
+    page = url.read().decode('ascii')
+    soup = bs4.BeautifulSoup(page)
     raw = soup.get_text(page)
-    text = re("[\t\n] +",' ',raw)
+    text = re.sub("[\t\n ]+",' ',raw)
     print text
 
-getResult("Penguins")
+getResult("penguins")
