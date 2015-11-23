51e29c730cd35107da4b6aac6c270fce99c66ef3
shishu1998 <shishu_1998@yahoo.com>
2015-11-22 20:02:31 -0500

    date stuff

--- utils.py
+++ utils.py
@@ -16,37 +16,52 @@ def getResult(query):
         """
         finds all text with capitals for 2 consecutive words
         """
-        text = re.findall("[A-Z][a-z] + [A-Z][a-z]+",raw)
+        text = re.findall("[A-Z][a-z]+ [A-Z][a-z]+",raw)
+        person = makedict(text)
+        return findResult(person)
     elif("where" in query):
         """
         finds all text with one capital
         """
         text = re.findall("[A-Z][a-z]+",raw)
+        place = makedict(text)
+        return findResult(place)
     else:
         """
-        not sure how to do when yet
+        makes 3 dictionaries for month,day,year
         """
-        text = re.findall(" +",raw)
-
-    dictionary = {}
-    for result in text:
-        """
-        if the result has been found before, add 1 to number of occurence
-        """
-        if result in dictionary.keys():
-            dictionary[result] += 1
-        """
-        if the result hasn't been found before, set the occurence equal to one
-        """
-        else:
-            dictionary[result] = 1
-    """
-    testing purposes
-    """
-    for key in dictionary.keys():
-        print "%s:%d" % (key,dictionary[key])
-    return findResult(dictionary)
+        text = re.findall("[A-z][a-z]+",raw)
+        text2 = re.findall("[0-3]?[[0-9]",raw)
+        text3 = re.findall("[0-2]?[0-9]{3}",raw)
+        month = makedict(text)
+        day = makedict(text2)
+        year = makedict(text3)
+        return findResult(month) + "," +  findResult(day) + "," + findResult(year)
         
+"""
+makes a dictionary for all the search results
+"""
+def makedict(list):
+    dictionary = {}
+    for item in list:
+        """
+        if the item has been found before, add 1 to number of occurence
+        """
+        if item in dictionary.keys():
+            dictionary[item] += 1
+            """
+            if the item hasn't been found before, set the occurence equal to one
+            """
+        else:
+            dictionary[item] = 1
+        
+        """
+        testing purposes
+        """
+        for key in dictionary.keys():
+            print "%s:%d" % (key,dictionary[key])
+    return dictionary
+
 def findResult(dictionary):
     result = dictionary.keys()[0]
     for key in dictionary.keys():
@@ -55,4 +70,4 @@ def findResult(dictionary):
     return result
 
 #getResult("Penguin")
-findResult({"a":1,"b":2,"c":3})
+print findResult({"a":1,"b":2,"c":3})
