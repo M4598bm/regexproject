49595554fcf5e3b66079eb659a825422f925b8e6
shishu1998 <shishu_1998@yahoo.com>
2015-11-26 11:34:05 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -40,7 +40,7 @@ def getResult(query):
             """
             makes 3 dictionaries for month,day,year
             """
-            text += re.findall("[JFMASOND][a-z]+",raw[100:300])
+            text += re.findall("(?!Day)[JFMASOND][a-z]+",raw[100:300])
             text2 += re.findall("[0-3]?[[0-9]",raw[100:300])
             text3 += re.findall("[0-2][0-9]{3}",raw[100:300])
         i += 1
